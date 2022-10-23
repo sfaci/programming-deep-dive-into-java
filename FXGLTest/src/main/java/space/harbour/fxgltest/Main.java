0049cade1cd39b04c33ad1d8e37d3ff009af61fd
Santiago Faci <>
2022-10-23 21:49:22 +0200

    Some improvements: sound, collisions, enemies, HUD

--- FXGLTest/src/main/java/space/harbour/fxgltest/Main.java
+++ FXGLTest/src/main/java/space/harbour/fxgltest/Main.java
@@ -2,32 +2,78 @@ package space.harbour.fxgltest;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.components.CollidableComponent;
 import com.almasb.fxgl.input.Input;
 import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.physics.CollisionHandler;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Main extends GameApplication {
 
     private Entity player;
+    private List<Entity> enemies;
+    private int score;
+    private enum EntityType {
+        PLAYER, ENEMY
+    }
 
     @Override
     protected void initSettings(GameSettings settings) {
         settings.setWidth(800);
         settings.setHeight(600);
         settings.setTitle("Basic Game App");
+        settings.setVersion("0.1");
+    }
+
+    @Override
+    protected void initUI() {
+        Text scoreText = FXGL.getUIFactoryService().newText("", Color.BLACK, 24);
+        scoreText.setTranslateX(50);
+        scoreText.setTranslateY(100);
+        scoreText.textProperty().bind(
+                new ReadOnlyStringWrapper("Score: ").concat(
+                        FXGL.getWorldProperties().intProperty("score").asString()
+                ));
+        FXGL.getGameScene().addUINode(scoreText);
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("score", 0);
     }
 
     @Override
     protected void initGame() {
         player = FXGL.entityBuilder()
-                .at(300, 300)
-                .view(new Rectangle(25, 25, Color.BLUE))
+                .type(EntityType.PLAYER)
+                .at(300, FXGL.getSettings().getHeight() - 25)
+                .viewWithBBox(new Rectangle(25, 25, Color.BLUE))
+                .with(new CollidableComponent(true))
                 .buildAndAttach();
 
+        enemies = new ArrayList<>();
+
+        FXGL.getGameTimer().runAtInterval(() -> {
+            Entity newEnemy = FXGL.entityBuilder()
+                    .type(EntityType.ENEMY)
+                    .at(FXGLMath.random(0, FXGL.getSettings().getWidth()), 0)
+                    .viewWithBBox(new Rectangle(50, 50, Color.RED))
+                    .with(new CollidableComponent(true))
+                    .buildAndAttach();
+            enemies.add(newEnemy);
+        }, Duration.seconds(0.5));
     }
 
     @Override
@@ -47,6 +93,34 @@ public class Main extends GameApplication {
                 player.translateX(-5); // move right 5 pixels
             }
         }, KeyCode.A);
+    }
+
+    @Override
+    protected void initPhysics() {
+        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityType.ENEMY,
+                EntityType.PLAYER) {
+
+            @Override
+            protected void onCollision(Entity enemy, Entity player) {
+                enemy.removeFromWorld();
+                score += 1;
+                FXGL.getWorldProperties().setValue("score", score);
+                FXGL.play("blop.mp3");
+            }
+        });
+    }
+
+    @Override
+    protected void onUpdate(double delta) {
+        super.onUpdate(delta);
+
+        for (Entity enemy : enemies) {
+            enemy.translateY(10);
+
+            if (enemy.getY() > FXGL.getSettings().getHeight()) {
+                enemy.removeFromWorld();
+            }
+        }
     }
 
     public static void main(String[] args) {
